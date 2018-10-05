3a14be31dd81c9af3e26f27d3cd460fa4e509535
BSJonas <jonasbsousa@hotmail.com>
2018-10-05 20:38:48 -0300

    Subindo arquivos

---
+++ src/main/java/com/example/controlmoney/api/ControlmoneyaApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.controlmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlmoneyaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlmoneyaApiApplication.class, args);
+	}
+}
